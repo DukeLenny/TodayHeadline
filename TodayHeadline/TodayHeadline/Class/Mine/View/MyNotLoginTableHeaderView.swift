522fe64be1c92d0e84b96be44fb0c2eafe827274
DukeLenny <1637549689@qq.com>
2018-11-23 17:19:32 +0800

    我的-未登录-列表头部视图收藏历史夜间按钮搭建.

---
+++ TodayHeadline/TodayHeadline/Class/Mine/View/MyNotLoginTableHeaderView.swift
@@ -0,0 +1,28 @@
+//
+//  MyNotLoginTableHeaderView.swift
+//  TodayHeadline
+//
+//  Created by LiDinggui on 2018/11/23.
+//  Copyright © 2018年 LiDinggui. All rights reserved.
+//
+
+import UIKit
+
+class MyNotLoginTableHeaderView: UIView {
+
+    @IBOutlet private weak var collectionButton: UIButton!
+    @IBOutlet private weak var historyButton: UIButton!
+    @IBOutlet private weak var nightButton: UIButton!
+    
+    @objc @IBAction private func collectionButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @objc @IBAction private func historyButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @objc @IBAction private func nightButtonClicked(_ sender: UIButton) {
+        
+    }
+}

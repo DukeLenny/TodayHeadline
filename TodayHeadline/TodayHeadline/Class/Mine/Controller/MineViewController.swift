f48d24ffd1a44bd80262e542c718cb3d19a9f579
DukeLenny <1637549689@qq.com>
2019-01-09 15:22:57 +0800

    隐藏"我的"界面导航条.

--- TodayHeadline/TodayHeadline/Class/Mine/Controller/MineViewController.swift
+++ TodayHeadline/TodayHeadline/Class/Mine/Controller/MineViewController.swift
@@ -40,13 +40,7 @@ class MineViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
 }
